56dc1904c13ded8a255368285575f922c6a32543
SagaXXI <77396998+SagaXXI@users.noreply.github.com>
2021-07-07 15:28:58 +0600

    cross product (i.e. tank turn mechanics)

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -32,9 +32,17 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	//AI tank's movememnt intention (where it should move)
 	FVector AIForwardIntention = MoveVelocity.GetSafeNormal();
 
+	//Getting the dot product of two vectors, for example if player tank is on the right-forward side (diagonal) of ai tank, it will find
+	//the dot product of both given vectors and final value will be 1, then it will need to go forward to be on the same line where player tank is.
 	float ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
-
 	IntendMoveForward(ForwardThrow);
+
+	//Getting the cross product of two vectors, so if player tank is staying on the right side of ai tank, it will find the cross product between
+	//both given vectors, and find the Z vector of it, because cross product returns fvector
+	float RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
+	
+
 	
 	
 } 
